f0aa89f68dfd537279d65303cdd8af7b2f7ea06a
Artemii <angap4@gmail.com>
2023-07-25 21:48:13 +1000

    lesson_2 полное решение
    Можно ли как-то функцией map выделить первый элемент?
    Почему .append и += не эквивалентны?

--- lesson_2/main.py
+++ lesson_2/main.py
@@ -2,6 +2,7 @@
 Второй урок
 """
 import math
+import sys
 from collections import defaultdict
 
 lowest_sym = ord('A')
@@ -46,8 +47,7 @@ def check_pin(pin_code: list[str]) -> None:
 
 
 if __name__ == '__main__':
-    case = input()
-    match case:
+    match input():
         case 'B':
             values = [1, 3, 1, 5, 7]
             operation = lambda num: num + 5
@@ -67,3 +67,37 @@ if __name__ == '__main__':
             check_pin(input().split("-"))
             # 7-101-4
             # 12-22-16
+        case 'P':
+            line = sys.stdin.readline().replace("\n", "")
+            words = defaultdict(int)
+            while line:
+                for word in line.split():
+                    words[word] += 1
+                line = sys.stdin.readline().replace("\n", "")
+            print("\n".join(dict(sorted(words.items(), key=lambda x: (-x[1], x[0]))).keys()))
+        case 'Q':
+            count_countries = int(input())
+            countries = dict()
+            for _ in range(count_countries):
+                cities = list(input().split())  # Можно ли как-то функцией map выделить первый элемент?
+                for i in range(1, len(cities)):
+                    countries[cities[i]] = cities[0]  # Как упростить?
+
+            for _ in range(int(input())):
+                print(countries[input()])
+        case 'U':
+            transaction = sys.stdin.readline().replace("\n", "")
+            people = defaultdict(lambda: defaultdict(int))
+            while transaction:
+                # Тело условия цикла явно можно сделать подлиннее...
+                person, item, count = transaction.split()
+                # people[person].append((item, int(count)))  # Добавляет кортеж как набор элементов!
+                people[person][item] += int(count)  # Добавляет словарь как набор элементов!
+                # .append и += не эквивалентны
+                transaction = sys.stdin.readline().replace("\n", "")
+            people = dict(sorted(people.items(), key=lambda x: (x[0])))
+            for human in people:
+                print(human + ":")
+                people[human] = dict(sorted(people[human].items(), key=lambda x: (x[0])))
+                for purchase in people[human]:
+                    print(purchase, people[human][purchase])
